3c83d8911110e79f517e695a79daf8bc4d985f2c
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-05-18 13:43:29 -0300

    Configured router. Created default layout. Created pages.

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 70rem;
+  height: calc(100vh - 2rem);
+  margin: 0 auto;
+  padding: 1rem;
+
+  display: flex;
+  flex-direction: column;
+`
