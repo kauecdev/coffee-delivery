9263c1c219ad81ad70d94243ebadf9487e2a6b48
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2024-05-18 13:29:52 -0300

    Configured application style theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <div></div>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
 }
